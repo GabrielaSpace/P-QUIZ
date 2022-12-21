e3330f4f2af050971724580d7fda0723932b1ae8
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-21 11:18:02 +0100

    preguntas

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,87 +2,93 @@ const questions = [
     {
         'question': '¿Cuál es el río más largo del mundo?',
         'option1': 'Nilo',
-        'option2': '4',
-        'option3': '7',
-        'option4': '2',
+        'option2': 'Eufrates',
+        'option3': 'Orinoco',
+        'option4': 'Amazonas',
+        'correct': 'Nilo'
 
 
     },
     {
         'question': '¿En qué continente está Ecuador?',
-        'option1': '3',
-        'option2': '4',
+        'option1': 'África',
+        'option2': 'Europa',
         'option3': 'América',
-        'option4': '2',
+        'option4': 'Asia',
+        'correct': 'América'
+
     },
     {
         'question': ' ¿Quién es el autor de "El Quijote"?',
-        'option1': '3',
+        'option1': 'José Saramago',
         'option2': 'Miguel de Cervantes',
-        'option3': '7',
-        'option4': '2',
+        'option3': 'Victor Hugo',
+        'option4': 'Edgar Allan Poe',
+        'correct': 'Miguel de Cervantes'
+        
     },
     {
         'question': '¿Dónde se encuentra la Sagrada Familia?',
         'option1': 'Barcelona',
-        'option2': '4',
-        'option3': '7',
-        'option4': '2',
+        'option2': 'Madrid',
+        'option3': 'Zaragoza',
+        'option4': 'Valencia',
+        'correct': 'Barcelona'
     },
     {
         'question': '¿Dónde se encuentra la Torre Eiffel?',
-        'option1': '3',
-        'option2': '4',
-        'option3': '7',
+        'option1': 'Lisboa',
+        'option2': 'Londres',
+        'option3': 'Toulouse',
         'option4': 'Paris',
+        'correct': 'Paris'
+
     },
     {
         'question': '¿Cuál es la moneda del Reino Unido?',
-        'option1': '3',
-        'option2': '4',
-        'option3': '7',
+        'option1': 'El Peso',
+        'option2': 'El Euro',
+        'option3': 'El Dólar',
         'option4': 'La Libra',
+        'correct': 'La Libra'
+
     },
     {
         'question': ' ¿Cual es país más poblado de la Tierra?',
-        'option1': '7',
-        'option2': '4',
-        'option3': '7',
+        'option1': 'India',
+        'option2': 'Estados Unidos',
+        'option3': 'Brasil',
         'option4': 'China',
+        'correct': 'China'
+
     },
     {
         'question': '¿A qué país pertenece la ciudad de Varsovia?',
-        'option1': '3',
+        'option1': 'Rusia',
         'option2': 'Polonia',
-        'option3': '7',
-        'option4': '2',
+        'option3': 'Serbia',
+        'option4': 'Albania',
+        'correct': 'Polonia'
+
     },
     {
         'question': ' ¿Cómo se llama el proceso por el cual las plantas obtienen alimento?',
-        'option1': '10',
-        'option2': '4',
+        'option1': 'Osmosis',
+        'option2': 'Transpiración',
         'option3': 'Fotosíntesis',
-        'option4': '2',
+        'option4': 'traslocación',
+        'correct': 'Fotosíntesis'
     },
     {
-        'question': '¿En qué país se usó la primera bomba atómica en combate?',
-        'option1': '3',
-        'option2': '4',
+        'question': '¿En qué país se usó la primera bomba atómica?',
+        'option1': 'Alemania',
+        'option2': 'Estados Unidos',
         'option3': 'Japón',
-        'option4': '2',
+        'option4': 'Israel',
+        'correct': 'Japón'
+
     }]
       
-let correctAnswers ={
-    correctAnswer1:'a',
-    correctAnswer2:'c',
-    correctAnswer3:'b',
-    correctAnswer4:'a',
-    correctAnswer5:'d',
-    correctAnswer6:'d',
-    correctAnswer7:'b',
-    correctAnswer8:'c',
-    correctAnswer9:'c'
-};
 
 let qheader=document.createElement('header');
 document.body.appendChild(qheader);
@@ -90,9 +96,9 @@ document.body.appendChild(qheader);
 let qMain= document.createElement('main');
 document.body.appendChild(qMain);
 
-let qSection = document.createElement('form');
-qSection.setAttribute('id','QuizForm')
-qMain.appendChild(qSection);
+let qForm = document.createElement('form');
+qForm.setAttribute('id','QuizForm')
+qMain.appendChild(qForm);
 
 
 for (let i =0; i <questions.length;i++){
@@ -103,81 +109,65 @@ for (let i =0; i <questions.length;i++){
 function allQuestions(object,i) {
 
 
-let qArticle = document.createElement('fieldset');
-qArticle.setAttribute('class','qcontainer');
-qSection.appendChild(qArticle);
-let qQuestion = document.createElement('label');
+let qFieldset = document.createElement('fieldset');
+qFieldset.setAttribute('class','qcontainer');
+qForm.appendChild(qFieldset);
+let qQuestion = document.createElement('legend');
 qQuestion.setAttribute('class','question');
-qArticle.appendChild(qQuestion);
 qQuestion.id= `question${i+1}`;
+qFieldset.appendChild(qQuestion);
 
+//hasta aqui funciona
 
-let qOption1Container= document.createElement('div');
-qOption1Container.setAttribute('class', 'option');
-qArticle.appendChild(qOption1Container);
-let qOption1 = document.createElement('label');
-qOption1Container.appendChild(qOption1);
-qOption1.setAttribute('class','option1');
-qOption1.setAttribute('for',`a${i}`);
+//opción A
+
 let aOption1 = document.createElement('input');
 aOption1.name=`question${i+1}`;
 aOption1.setAttribute('type','radio');
-aOption1.id=`a${i}`;
-aOption1.value='a';
-aOption1.setAttribute('onclick', `respuesta(${i+1},this)`);
-qOption1Container.appendChild(aOption1);
+aOption1.id=`a${i+1}`;
+qFieldset.appendChild(aOption1);
 
-let qOption2Container= document.createElement('div');
-qOption2Container.setAttribute('class', 'option');
-qArticle.appendChild(qOption2Container);
-let qOption2 = document.createElement('label');
-qOption2Container.appendChild(qOption2);
-qOption2.setAttribute('class','option2');
-qOption2.setAttribute('for',`b${i}`);
+let qOption1 = document.createElement('label');
+qFieldset.appendChild(qOption1);
+qOption1.setAttribute('class','option1');
+qOption1.setAttribute('for',`a${i+1}`); 
+
+//opción B
 let aOption2 = document.createElement('input');
-aOption2.name= `question${i+1}`;
+aOption2.name=`question${i+1}`;
 aOption2.setAttribute('type','radio');
-aOption2.id=`b${i}`;
-aOption2.value='b';
-aOption2.setAttribute('onclick', `respuesta(${i+1},this)`);
-qOption2Container.appendChild(aOption2);
+aOption2.id=`b${i+1}`;
+qFieldset.appendChild(aOption2);
+
+let qOption2 = document.createElement('label');
+qFieldset.appendChild(qOption2);
+qOption2.setAttribute('class','option2');
+qOption2.setAttribute('for',`b${i+1}`); 
 
 
-let qOption3Container= document.createElement('div');
-qOption3Container.setAttribute('class', 'option');
-qArticle.appendChild(qOption3Container);
-let qOption3 = document.createElement('label');
-qOption3Container.appendChild(qOption3);
-qOption3.setAttribute('class','option3');
-qOption3.setAttribute('for',`c${i}`);
+//opción C
 let aOption3 = document.createElement('input');
-aOption3.name= `question${i+1}`;
+aOption3.name=`question${i+1}`;
 aOption3.setAttribute('type','radio');
-aOption3.id=`c${i}`;
-aOption3.value='c';
-aOption3.setAttribute('onclick', `respuesta(${i+1},this)`);
-qOption3Container.appendChild(aOption3);
+aOption3.id=`c${i+1}`;
+qFieldset.appendChild(aOption3);
 
-let qOption4Container= document.createElement('div');
-qOption4Container.setAttribute('class', 'option');
-qArticle.appendChild(qOption4Container);
-let qOption4 = document.createElement('label');
-qOption4Container.appendChild(qOption4);
-qOption4.setAttribute('class','option4');
-qOption4.setAttribute('for',`d${i}`);
+let qOption3 = document.createElement('label');
+qFieldset.appendChild(qOption3);
+qOption3.setAttribute('class','option3');
+qOption3.setAttribute('for',`c${i+1}`); 
+
+//Opción D
 let aOption4 = document.createElement('input');
-aOption4.name= `question${i+1}`;
+aOption4.name=`question${i+1}`;
 aOption4.setAttribute('type','radio');
-aOption4.id= `d${i}`;
-aOption4.value='d';
-aOption4.setAttribute('onclick', `respuesta(${i+1},this)`);
-qOption4Container.appendChild(aOption4); 
+aOption4.id=`d${i+1}`;
+qFieldset.appendChild(aOption4);
 
-/* qQuestion.textContent= object.question;
-qOption1.textContent = object.Option1;
-qOption2.textContent = object.Option2;
-qOption3.textContent = object.Option3;
-qOption4.textContent = object.Option4; */
+let qOption4 = document.createElement('label');
+qFieldset.appendChild(qOption4);
+qOption4.setAttribute('class','option4');
+qOption4.setAttribute('for',`d${i+1}`); 
 
 
 qQuestion.textContent= object.question;
@@ -187,36 +177,23 @@ document.getElementsByClassName('option3')[i].innerHTML = object.option3;
 document.getElementsByClassName('option4')[i].innerHTML = object.option4;
 }
 
-
-let boton = createElement('button');
+let boton = document.createElement('button');
 boton.setAttribute('type','submit');
-boton.value='Enviar respuestas';
-qSection.appendChild(boton);
+boton.textContent='Submit';
+qForm.appendChild(boton);
 
 
+    //validación por pregunta 
+            let inputs = document.querySelectorAll('input[type=radio]:checked');
+            let values=[];
+            let correctAnswersCounter=0;
 
+            for(input of inputs){
+                values.push(input.value)
+            }
+            if(values.includes(questions.correct)){
+                correctAnswersCounter++;
+                }
 
-
-document.querySelector('form').addEventListener("submit", function(event) {
-    event.preventDefault();
-    let correctAnswers = ['a','c','b','a','d','d','b','c','c','d']
-    let userAnswers = [];
-    let answerXQ =[];
-    let correctAnswersCounter =0;
-    let incorrectAnswersCounter =0;
-    let questionsAnswered =0;
-    
-    for (let i = 0; i < correctAnswers.length; i++) {
-        answerXQ =document.getElementsByName(`question${i+1}`);
-        for (let j=0;j<=3;j++ ){
-            if (answerXQ[j].checked===true){
-                questionsAnswered++;
-                userAnswers[i]=answerXQ[j].value;
-                answerXQ[j].value== correctAnswers[i]? correctAnswersCounter++: incorrectAnswersCounter++  
-                document.getElementById('score').innerHTML = correctAnswersCounter;
-                document.getElementById('score').innerHTML = incorrectAnswersCounter;
-                questionsAnswered==10 ? document.write('has respondido todo'): document.write(`te faltaron por responder ${10-questionsAnswered}`);
-            }}
-        } 
-    } 
-)
+            let score =document.querySelector('i');
+            score.innerHTML = correctCounter +'/10';
